f84cae53ce60ec01030dc60d67b2827b425db8cc
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-21 15:52:17 -0500

    Changing some of the files around to better organize them

--- pythonProject/Python Notes/PythonMathBasics.py
+++ pythonProject/Python Notes/PythonMathBasics.py
@@ -1,20 +1,10 @@
 # Python Data Types
-int
-float
-bool
-str
-list
-tuple
-set
-dict
+int, float, bool, str, list, tuple, set, dict
 
 # These are places to store specific types of information, we can manipulate this data
 # These are the fundamental data types
-
 # Classes -> these are custom types
-
 # Specialized Data Types -> special packages and modules we can use (extensions to the language)
-
 # None -> means nothing, idea of zero in math
 
 # Fundamental Data Types
@@ -38,3 +28,4 @@ print(abs(-20)) # Absolute value of -20
 # Operator Precedence
 print(20 - 3 * 4) # Prints 8 because -> order of operations
 # () ---> ** ----> * , / ---> + , -
+
