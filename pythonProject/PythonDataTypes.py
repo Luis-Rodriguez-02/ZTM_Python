154412b15e06c16112cf1091d02f9f981fdd15f1
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-21 12:24:51 -0500

    Python Math Functions

--- pythonProject/PythonDataTypes.py
+++ pythonProject/PythonDataTypes.py
@@ -18,17 +18,19 @@ dict
 # None -> means nothing, idea of zero in math
 
 # Fundamental Data Types
-
 print(2 + 4)
 print(2 - 4)
 print(2 *  4)
 print(2 / 4)
 
 # We can use the type() method to check the type
-
 print(type(2 + 4))
 print(type(2 / 4)) # 0.5, floating point number -> number with decimal point
 print(2 ** 2) # This means power of, its exponent
 print(2 // 4) # Returns an integer rounded down
 print(5 % 4) # Returns the remainder of division
 
+# Math functions -> functions are actions that we can perform on the data
+print(round(3.1)) # Rounds down
+print(round(3.9)) # Rounds up
+print(abs(-20)) # Absolute value of -20
