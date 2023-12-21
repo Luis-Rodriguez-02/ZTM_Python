5e1a827c50dcad4baba35c826f908532ca698666
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-21 12:20:03 -0500

    Python Numbers Video

--- pythonProject/PythonDataTypes.py
+++ pythonProject/PythonDataTypes.py
@@ -16,3 +16,19 @@ dict
 # Specialized Data Types -> special packages and modules we can use (extensions to the language)
 
 # None -> means nothing, idea of zero in math
+
+# Fundamental Data Types
+
+print(2 + 4)
+print(2 - 4)
+print(2 *  4)
+print(2 / 4)
+
+# We can use the type() method to check the type
+
+print(type(2 + 4))
+print(type(2 / 4)) # 0.5, floating point number -> number with decimal point
+print(2 ** 2) # This means power of, its exponent
+print(2 // 4) # Returns an integer rounded down
+print(5 % 4) # Returns the remainder of division
+
