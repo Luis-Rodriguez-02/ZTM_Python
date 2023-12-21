1e207fb2b32d5cb595fbb0a5e9d073ed9e2d760b
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-21 11:28:32 -0500

    intro to python data types

---
+++ pythonProject/PythonDataTypes.py
@@ -0,0 +1,18 @@
+# Python Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# These are places to store specific types of information, we can manipulate this data
+# These are the fundamental data types
+
+# Classes -> these are custom types
+
+# Specialized Data Types -> special packages and modules we can use (extensions to the language)
+
+# None -> means nothing, idea of zero in math
