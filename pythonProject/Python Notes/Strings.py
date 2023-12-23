5283822652cee5c602d190e0924dd5d0cb0a4b19
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-22 21:25:11 -0500

    Strings in Python

---
+++ pythonProject/Python Notes/Strings.py
@@ -0,0 +1,23 @@
+#Strings in Python!
+
+'hi hello there'  # this is a string
+"hello"  # this is also a string
+
+print("Hello")
+user_name = 'supercoder'
+password = 'supersecret'
+
+long_string  = '''
+WOW THIS IS A VERRY LONG STRING
+O O
+---
+'''
+
+print(long_string)
+
+first_name = 'Luis'
+last_name = 'Rodriguez'
+full_name = first_name + " " + last_name
+print(full_name)
+
+
