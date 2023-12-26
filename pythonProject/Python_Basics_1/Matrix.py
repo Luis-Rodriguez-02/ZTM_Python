bacaa3adca6e65f3f726bdb691afa15579356df2
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-26 18:35:40 -0500

    Matrix and Matrix exercise

---
+++ pythonProject/Python_Basics_1/Matrix.py
@@ -0,0 +1,15 @@
+# Matrix -> 2D Lists -> similar to 2D Arrays in Java and C++
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ]
+
+print(matrix[0][1])  # Same as Java and C++ accessing syntax
+
+# Exercise
+# using this list:
+basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+# access "Oranges" and print it:
+
+print(basket[1][1][0])
