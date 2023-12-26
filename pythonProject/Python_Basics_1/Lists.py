b6b4e7b563eb134e321b91fce24d38bcb0239732
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-26 17:31:26 -0500

    Lists

---
+++ pythonProject/Python_Basics_1/Lists.py
@@ -0,0 +1,13 @@
+# Lists -> ordered sequence of objects of any type
+
+li = [1, 2, 3, 4, 5]
+li2 = ['a', 'b', 'c']
+li3 = [1, 2.6, 'a', True]
+
+
+amazon_cart = ['notebooks', 'sunglasses']
+print(amazon_cart[0])
+print(amazon_cart[::-1])
+
+# Data Structures -> Way to organize different data -> container
+
