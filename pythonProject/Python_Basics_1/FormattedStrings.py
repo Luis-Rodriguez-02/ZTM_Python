7d17f782a467ea2d5d03374576f2536e6ed383a3
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-26 12:12:00 -0500

    Formatted Strings

---
+++ pythonProject/Python_Basics_1/FormattedStrings.py
@@ -0,0 +1,12 @@
+# Formatted Strings
+
+# We want a dynamic display
+name = 'Luis'  # Grab some user information
+age = 55
+
+print('Hi' + name)
+print('Hi ' + name + ". You are " + str(age) + ' years old.')
+
+# To format add and f
+print(f'hi {name}. You are {age} years old')
+
