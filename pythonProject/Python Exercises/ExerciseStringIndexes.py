afc9338ae29510ba6fc3e16bdbf62a800586ec03
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-26 13:32:59 -0500

    String index and slicing exercise

---
+++ pythonProject/Python Exercises/ExerciseStringIndexes.py
@@ -0,0 +1,12 @@
+#Guess the output of each print statement before you click RUN!
+python = 'I am PYHTON'
+        # 012345678910
+print(python[1:4])  # am
+print(python[1:])  # am PYHTON
+print(python[:])  # I am PYHTON
+print(python[1:100]) # Error -> not correct prints from 1 to end of String
+print(python[-1])  # N
+print(python[-4])  # Y -> starts from -1 index not 0 so, H
+print(python[:-3])  # Start 0 - > Stops at -3 which is T
+print(python[-3:])  # TON
+print(python[::-1])  # NOTHYP MA I
