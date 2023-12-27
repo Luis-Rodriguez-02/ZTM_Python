b937dd78298891d1b7fb8569f1d70717f674ee3c
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-27 07:22:40 -0500

    List patterns and exercise

---
+++ pythonProject/Python_Basics_1/CommonListPatterns.py
@@ -0,0 +1,17 @@
+basket = ['a', 'b', 'c', 'd', 'e', 'f']
+basket.sort()  # sorts the list
+basket.reverse()  # reverses the list
+print(basket[::-1])
+
+print(list(range(100)))
+
+# sentence = ' '
+new_sentence = ' '.join(['Hi', 'my', 'name', 'is', 'luis'])
+print(new_sentence)
+
+#fix this code so that it prints a sorted list of all of our friends (alphabetical). Scroll to see answer
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+print(sorted(friends) + new_friend)
