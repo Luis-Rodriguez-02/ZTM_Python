9e7e44ffd9dee3f4619d47aca42a496f3873a9f8
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-29 15:13:27 -0500

    Tuples

---
+++ pythonProject/Python_Basics_1/Tuples.py
@@ -0,0 +1,8 @@
+# Tuple
+# Like a list -> but immutable (can't change values)
+# If you don't need to change the list use it -> code predictable, less flexible than a list
+# Usually faster than list
+
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple[2])
+print(5 in my_tuple)
