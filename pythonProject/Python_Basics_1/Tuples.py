38b94d4d69fca722840947098bcfef8ed4fb5420
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-29 16:54:22 -0500

    Tuples 2

--- pythonProject/Python_Basics_1/Tuples.py
+++ pythonProject/Python_Basics_1/Tuples.py
@@ -6,3 +6,19 @@
 my_tuple = (1, 2, 3, 4, 5)
 print(my_tuple[2])
 print(5 in my_tuple)
+
+# Tuples 2
+
+new_tuple = my_tuple[:]
+print(new_tuple)
+
+x = my_tuple[0]
+x, y, z, *other = (5, 5, 9, 10, 11, 200)
+
+print(x)
+print(other)
+
+
+# count and index
+print(my_tuple.count(5))
+my_tuple.index(5)
