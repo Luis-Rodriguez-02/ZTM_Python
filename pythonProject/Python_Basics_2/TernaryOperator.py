3cc93fc55ae816fadfaf46f29df62f89e1f3f93a
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-31 20:54:27 -0500

    Ternary operator

---
+++ pythonProject/Python_Basics_2/TernaryOperator.py
@@ -0,0 +1,10 @@
+# Ternary Operator
+
+# condition_if_true if condtition  else condition_if_else
+
+
+# Determine if user is friend
+is_friend = False
+can_message = "message allowed" if is_friend else "message not allowed"
+
+print(can_message)
