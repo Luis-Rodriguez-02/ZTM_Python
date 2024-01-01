1dd3fc334e0ce0852b53a433227f305cb45c7653
lrodriguez114 <kylereaglegale@gmail.com>
2023-12-31 19:10:47 -0500

    Conditional logic

---
+++ pythonProject/Python_Basics_2/ConditionalLogic.py
@@ -0,0 +1,9 @@
+is_old = False
+is_licenced = False
+
+if is_old and is_licenced:
+    print("You are old enough to drive and have a license")
+else:
+    print("you are not of age")
+
+print("okok")
