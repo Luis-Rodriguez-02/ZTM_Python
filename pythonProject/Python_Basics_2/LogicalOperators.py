11700109255fa0effcfb17c409ecdf25e59aec97
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-01 16:31:16 -0500

    Logical Operators

---
+++ pythonProject/Python_Basics_2/LogicalOperators.py
@@ -0,0 +1,22 @@
+# Logical operators
+
+# > -> greater than
+print(4 > 5)
+# < -> less than
+print(4 < 5)
+# == -> equality
+print(4 == 5)
+
+# <= -> less than or equal to
+print(5 <= 4)
+# >= greater than or equal to
+print(5 >= 4)
+# != -> not equal to
+print(5 != 4)
+
+# not -> opposite
+print(not(True))
+# and -> both
+print(True and True)
+# or -> either
+print(True or False)
--- pythonProject/Python_Basics_2/ShortCircuiting.py
+++ pythonProject/Python_Basics_2/ShortCircuiting.py
@@ -3,7 +3,7 @@
 is_friend = True
 is_user = True
 
-# Short circuitng can be done when we use something like or
+# Short circuiting can be done when we use something like or
 # when we do or if the first condition is true second val is not checked
 if True or is_user:
     print(is_friend and is_user)
@@ -11,3 +11,4 @@ if True or is_user:
 # Works with and if the first val is false
 if False and True:
     print("Never printed and short circuit")
+
