d7ca473e1853c4ba8625eccdc5df265f3d30b95a
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-01 17:10:15 -0500

    For loops

---
+++ pythonProject/Python_Basics_2/ForLoops.py
@@ -0,0 +1,17 @@
+# looping
+
+for letters in 'Zero to Mastery':  # 'Zero to Mastery is an iterable, letters is a variable
+    print(letters)
+
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+
+for even in {2, 4, 6, 8}:
+    print(even)
+
+for odd in (2, 3, 5, 7, 9):
+    print(odd)
+
+for item in [1, 2, 3, 4, 5]:
+    for x in ['a', 'b', 'c']:
+        print(item, x)
