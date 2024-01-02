dd02ff344921e7e608dda6a3e8cdbec02d413181
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-02 15:18:12 -0500

    break, continue, and pass

---
+++ pythonProject/Python_Basics_2/BreakContinuePass.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3]
+
+# break -> completely exits the loop
+# continue -> go back to the loop (i.e skip an iteration)
+# pass -> passes to the next line -> used as a placeholder usually
+
+for item in my_list:
+    print(item)
+    continue
