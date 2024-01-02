2d211d181c030a7ccde4086ddd231c0b39fdeff2
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-02 17:20:15 -0500

    ParametersAndArguments

---
+++ pythonProject/Python_Basics_2/ParametersAndArguments.py
@@ -0,0 +1,8 @@
+# Functions can be made dynamic using parameters
+# Arguments are the exact value provided to the function
+
+def say_hello(name, emoji):
+    print(f'Hello, {name}! {emoji}')
+
+
+say_hello('Luis', '😂')
