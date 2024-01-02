ba9b8facbd53e6f1aca55638994182d31c7adbd9
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-02 17:15:47 -0500

    Python functions

---
+++ pythonProject/Python_Basics_2/Functions.py
@@ -0,0 +1,13 @@
+# Functions -> things like print(), len(), bool(), input()
+def say_hello():
+    print("Hello")
+
+print(say_hello)  # Location in memeory of the function
+
+# Functions allow us to avoid repeating code
+# This allow us to easily repeat actions
+
+
+
+
+
