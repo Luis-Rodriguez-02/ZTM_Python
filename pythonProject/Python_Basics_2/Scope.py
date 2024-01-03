9fb71a5c3d5762efb0cf834a9d981da126855877
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-03 06:46:34 -0500

    Scope

---
+++ pythonProject/Python_Basics_2/Scope.py
@@ -0,0 +1,8 @@
+# Scope -> What variables do we have access to
+# function scope -> not global if declared in function
+def some_func():
+    z_total = 100
+    print(total)
+
+
+total = 100   # part of global scope
