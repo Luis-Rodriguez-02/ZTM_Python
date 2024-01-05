2151384f83d36312e52a03c703b2f78a8b087021
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-05 18:42:49 -0500

    OOP explained

---
+++ pythonProject/Python_OOP/WhatIsOOP.py
@@ -0,0 +1,10 @@
+# Everything in python is an object
+# All the data types have the class keyword
+# Obejcts have methods we can access with dot
+
+print(type(True))
+print(type(5))
+print(type([0]))  # all of these are classes
+
+# OOP is a paradigm -> how to structure our code
+# helps with organization and modeling real world things
