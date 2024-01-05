c34081c20e1c4c6365af4270674dba4243600012
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-05 18:49:58 -0500

    OOP explained part 2

--- pythonProject/Python_OOP/WhatIsOOP.py
+++ pythonProject/Python_OOP/WhatIsOOP.py
@@ -8,3 +8,13 @@ print(type([0]))  # all of these are classes
 
 # OOP is a paradigm -> how to structure our code
 # helps with organization and modeling real world things
+
+# PART 2
+# Paradigms started coming out in the 70's
+# Before that we use procedural code
+# Now OOP is very popular -> real world models (blueprints)
+'''test class'''
+class BigObject:
+    pass
+
+obj1 = BigObject()
