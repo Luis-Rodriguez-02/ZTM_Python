5c087f2d1469eba39269f4e2cb6d7b1f921ea207
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-06 10:50:41 -0500

    dunder method exercises

---
+++ pythonProject/Python_Exercises/exercise_dunder_methods.py
@@ -0,0 +1,39 @@
+#By reading the python documentation, add 3 more magic/dunder methods of your choice to this Toy class. 
+class Toy():
+  def __init__(self, color, age):
+    self.color = color
+    self.age = age
+    self.my_dict = {
+        'name':'Yoyo',
+        'has_pets': False,
+    }
+
+  def __str__(self):
+    return f"{self.color}"
+
+  def __len__(self):
+    return 5
+
+  def __del__(self):
+    return "deleted"
+
+  def __call__(self):
+      return('yes??')
+
+  def __getitem__(self,i):
+      return self.my_dict[i]
+  
+  def __repr__(self) -> str:
+      print(f"This is a nice {self.color} colored toy!")
+
+  def __delattr__(self, __name: str) -> None:
+      return "name deleted"
+
+
+action_figure = Toy('red', 0)
+print(action_figure.__str__())
+print(str(action_figure))
+print(len(action_figure))
+print(action_figure())
+print(action_figure['name'])
+action_figure.__repr__()
