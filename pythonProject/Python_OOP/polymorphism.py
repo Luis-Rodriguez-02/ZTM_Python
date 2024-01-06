82440ef6d3f40c645e37b10b05cc7aee0c76abb9
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-06 10:04:52 -0500

    polymorphism

---
+++ pythonProject/Python_OOP/polymorphism.py
@@ -0,0 +1,7 @@
+# Enscapsulation - > packacking things
+# Abstraction - > hiding things
+# Inheritance - > sharing info
+# Polymorphism - > many forms
+
+# Object classes can share method names and act differently based on what calls them -> attack is shared but does different things based on the attributes
+# Wizard -> attacks with power, Archer -> attacks with arrows
