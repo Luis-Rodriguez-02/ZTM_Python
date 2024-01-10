18a9623b8ff6aa859a0d21cda927ae80092277e2
lrodriguez114 <kylereaglegale@gmail.com>
2024-01-09 19:48:13 -0500

    new topic

---
+++ pythonProject/Python_Functional_Programming/what_is_functional_programming.py
@@ -0,0 +1,3 @@
+# We separate data and the interations that act upon them in functional programming
+# Generally functional programs -> functions operate on data structures
+# PURE FUNCTIONS -> Separation of Data and behavior of functions
